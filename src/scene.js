311ce32c4e7176b20e31f73eb0776f2e921f84d6
customer <name41054@gmail.com>
2022-04-23 15:28:25 +0100

    first tesrt

---
+++ src/scene.js
@@ -0,0 +1,38 @@
+const scene = new THREE.Scene();
+
+
+// camera
+const camera = new THREE.PerspectiveCamera();
+camera.position.z = 5;
+
+
+// renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+// geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+
+// material
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+
+// mesh
+const mesh = new THREE.Mesh(geometry, material);
+
+
+// add mesh to scene
+scene.add(mesh);
+
+
+// rendering
+function render() {
+    requestAnimationFrame(render);
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+render()
